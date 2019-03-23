63e21c507929694444b758b030b9750ef9899041
jesus.castillo@hites.cl <jesus.castillo@hites.cl>
2019-03-23 12:50:21 -0300

    vista inscripcion inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { InscripcionComponent } from './components/inscripcion/inscripcion.component';
 
+
+
+import {MatFormFieldModule,MatInputModule} from '@angular/material'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    InscripcionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
